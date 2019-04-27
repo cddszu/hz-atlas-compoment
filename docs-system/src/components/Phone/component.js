a233e8f4d79bf06bf31f0f8c252d541b25268bad
Jamter Tsai <121978614@qq.com>
2019-04-27 18:33:05 +0800

    feat(hz Phone): 实现可移动、调整位置

--- docs-system/src/components/Phone/component.js
+++ docs-system/src/components/Phone/component.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './component.scss'
 import {withRouter} from 'react-router'
+import { Rnd } from 'react-rnd'
 class Phone extends React.Component {
   constructor(props) {
     super(props)
@@ -15,10 +16,18 @@ class Phone extends React.Component {
   }
 
   render() {
+    let initX = (window.document.documentElement.clientWidth - 256 ) * 0.7
+    let initY = window.document.documentElement.clientHeight  - 10
     return (
-      <div className='phone-component' draggable>
-        <iframe src={`http://localhost:18002/#/root/demos/${this.props.to}`}></iframe>
+      <div className='phone-component'>
+      <Rnd
+      >
+        <div className='phone-component-content'>
+          <iframe src={`http://localhost:18002/#/root/demos/${this.props.to}`}></iframe>
+        </div>
+      </Rnd>
       </div>
+
     )
   }
 }
